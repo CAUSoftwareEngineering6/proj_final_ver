40bf9b8f4c194af2d1d17bf7616d9d694978c9a4
segyugo <119419619+segyugo@users.noreply.github.com>
2024-06-18 21:13:53 +0900

    Update Debate.py

--- back/Debate.py
+++ back/Debate.py
@@ -8,8 +8,3 @@ class Debate(Post):
         self.post_id = str(Debate.next_id)
         Debate.next_id += 1
 
-    def delete_post(self):
-        pass
-
-    def update_postname(self, new_postname: str):
-        self.postname = new_postname
